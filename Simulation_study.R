39a4038d0a0cc6acb2444482c100a02f3b4a2ac7
peacel97 <leona-jung@gmx.de>
2022-05-19 14:10:00 +0200

    19.05.22

--- Simulation_study.R
+++ Simulation_study.R
@@ -1,13 +1,5 @@
 ####### V0 Simulation study
 
-# Threshold predicted class, normalize?
-# Change one continuous variable to binary?
-# External validation
-# Shrinkage
-# Interpretation difference complete data and amputed data too little?
-# Pseudo code for chapter 3
-# loop
-
 ######################################
 ### Install and load required packages
 ######################################
